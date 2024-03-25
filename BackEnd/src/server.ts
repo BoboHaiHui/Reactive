f8d1551fbc8f5d7c349db05b1a84a3ada877f96c
NEAGU Bogdan <bogdan.neagu@anais.digital>
2024-03-25 12:56:25 +0200

    set up angular

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -8,5 +8,5 @@ app.listen(port, ()=>{
   console.log(`Server is listening on port ${port}`);
 })
 app.get('/', (req, res)=>{
-  res.status(200).json({"status":"Success", "data":"Hello there"})
+  res.status(200).json({"status":"Success", "data":"Hello"})
 })
